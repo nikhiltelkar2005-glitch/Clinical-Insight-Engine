55207740189d618a03f263d97528b5cd5200e2e9
Gopal Gupta <gopaljilab@gmail.com>
2026-05-30 03:32:40 +0530

    chore: relax CSP for development

--- server/index.ts
+++ server/index.ts
@@ -69,6 +69,8 @@ app.use(
         defaultSrc: ["'self'"],
         scriptSrc: [
           "'self'",
+          "'unsafe-eval'",
+          "'unsafe-inline'",
           (_req: any, res: any) => `'nonce-${res.locals.cspNonce}'`,
         ],
         styleSrc: [
